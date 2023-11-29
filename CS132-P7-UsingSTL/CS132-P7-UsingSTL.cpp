7df700504ae63687ab890a1ffd17a1fea941765f
Taylor Bisset <taylorbisset@gmail.com>
2023-11-29 09:33:09 -0800

    Assignment header added to main.cpp

--- CS132-P7-UsingSTL/CS132-P7-UsingSTL.cpp
+++ CS132-P7-UsingSTL/CS132-P7-UsingSTL.cpp
@@ -1,3 +1,15 @@
+// main.cpp
+
+/*
+Name:          Taylor Bisset
+Section:       A
+Program Name:  CS132-Program6-TemplateClass
+
+Description:   
+
+Repo:           https://github.com/TaylorBisset/CS132-P7-UsingSTL 
+
+*/
 
 #include <iostream>
 
